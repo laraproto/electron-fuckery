bcb61d2e9a53a022e6f5dd7987fe9e4365542eef
laraproto <laraproto@proton.me>
2026-08-08 17:08:11 +0300

    just ignore the tests until i can get a macos release

--- electron-builder.mjs
+++ electron-builder.mjs
@@ -20,7 +20,7 @@ export default /** @type import('electron-builder').Configuration */
     syncDesktopName: true,
   },
   mac: {
-    target: ["dmg"],
+    target: ["dmg", "zip"],
     category: "public.app-category.utilities",
   },
   win: {
